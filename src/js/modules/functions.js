c2ac8fa809ee51225101242052facc4610f6bece
Dima Ponomar <dimaponomarenko443@gmail.com>
2022-02-09 00:25:53 +0300

    rebuild header, adaptive foter

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -29,16 +29,19 @@ export function global() {
 
   menuBox.style.paddingTop = String(headerHeight + 'px');;
 
-  let headerPopup = document.querySelector('.js-popup');
   let headerPopupClose = document.querySelector('.js-popup-close');
   let searchPopupLink = document.querySelector('.js-search-link');
+  let searchLinkAtribute = searchPopupLink.getAttribute('href').replace('#', '');
+  let currentPopup = document.getElementById(searchLinkAtribute);
 
   headerPopupClose.addEventListener('click', function () {
-    headerPopup.classList.remove('search-popup--active');
+    currentPopup.classList.remove('search-popup--active');
   })
 
-  searchPopupLink.addEventListener('click', function () {
-    headerPopup.classList.add('search-popup--active');
+  searchPopupLink.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    currentPopup.classList.add('search-popup--active');
   })
 
 
@@ -50,37 +53,22 @@ export function global() {
     if (w <= n && !minResize) {
       minResize = true;
       maxResize = false;
-      let adaptiveMenu = document.querySelector('.js-header__list');
-      let listItem = document.createElement('li');
-      let menuList = document.querySelector('.js-menulist');
-      let headerListItem = document.querySelectorAll('.js-header__list li.header__list-item');
-      let headerListLink = document.querySelectorAll('.js-header__list a.header__list-link');
-
-      for (let i = 0; i < headerListItem.length; i++) {
-        headerListItem[i].classList.remove('header__list-item');
-        headerListItem[i].classList.add('submenu__item');
-      }
-
-      for (let i = 0; i < headerListLink.length; i++) {
-        headerListLink[i].classList.remove('header__list-link');
-        headerListLink[i].classList.add('submenu__link');
-      }
-
-      listItem.className = 'menu__list-item js-listitem';
-      listItem.innerHTML = '<span class="menu__list-caption">Важные</span>';
-
-      adaptiveMenu.classList.remove('header__list');
-      adaptiveMenu.classList.add('submenu');
-
-
-      menuList.append(listItem);
-      document.querySelector('.js-listitem').append(adaptiveMenu);
 
       let siteContainer = document.querySelector('.js-wrapper');
 
+      if (burgerBtn.classList.contains('burger--active')) {
+        siteContainer.classList.add('site-container--lock');
+      } else {
+        siteContainer.classList.remove('site-container--lock');
+      }
+
       if (burgerBtn) {
         burgerBtn.addEventListener('click', function () {
-          siteContainer.classList.toggle('site-container--lock')
+          if (burgerBtn.classList.contains('burger--active')) {
+            siteContainer.classList.add('site-container--lock');
+          } else {
+            siteContainer.classList.remove('site-container--lock');
+          }
         })
       }
 
@@ -110,33 +98,11 @@ export function global() {
       maxResize = true;
       minResize = false;
 
-      let headerListItem = document.querySelectorAll('.js-header__list li.submenu__item');
-      let headerListLink = document.querySelectorAll('.js-header__list a.submenu__link');
-      let listItemMenu = document.querySelector('.js-listitem');
-      let adaptiveMenu = document.querySelector('.js-header__list');
+      let siteContainer = document.querySelector('.js-wrapper');
 
-      for (let i = 0; i < headerListItem.length; i++) {
-        headerListItem[i].classList.remove('submenu__item');
-        headerListItem[i].classList.add('header__list-item');
+      if (siteContainer.classList.contains('site-container--lock')) {
+        siteContainer.classList.remove('site-container--lock');
       }
-
-      for (let i = 0; i < headerListLink.length; i++) {
-        headerListLink[i].classList.remove('submenu__link');
-        headerListLink[i].classList.add('header__list-link');
-      }
-
-      adaptiveMenu.classList.remove('submenu');
-      adaptiveMenu.classList.add('header__list');
-
-
-      let headerLogo = document.querySelector('.header__logo');
-
-      headerLogo.insertAdjacentElement('afterend', adaptiveMenu)
-
-      if (listItemMenu) {
-        listItemMenu.remove();
-      }
-
       console.log("Window width > " + n);
     }
   };
